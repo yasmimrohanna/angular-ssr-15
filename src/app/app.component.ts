c95222e4f259fac20877a8ec151b69dc33e04b83
Yasmim e Jean <yasmimejean@MacBook-Air-de-Yasmim.local>
2025-05-07 11:00:01 -0300

    feat: hello world via ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer} from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-ssr-15';
+export class AppComponent implements OnInit {
+
+  private platformId: string;
+
+  todos: ToDo[] = [];
+  todosKey = makeStateKey<ToDo[]>("todos");
+
+  constructor(@Inject(PLATFORM_ID) platformId: string, private http: HttpClient, private transferState: TransferState) {
+    this.platformId = platformId;
+  }
+
+  ngOnInit(): void {
+    if (isPlatformServer(this.platformId)) {
+      this.http
+        .get<ToDo[]>('https://jsonplaceholder.typicode.com/todos')
+        .subscribe(todos => {
+          this.todos = todos.slice(0, 10);
+          this.transferState.set(this.todosKey, this.todos);
+        });
+    } else if (isPlatformBrowser(this.platformId)) {
+      this.todos = this.transferState.get(this.todosKey, []);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
